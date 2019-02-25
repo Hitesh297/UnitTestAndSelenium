5f10a4796e66a02fcb65bfed5ac5d0f55386dc72
Hitesh297 <hites_297@yahoo.co.in>
2019-02-25 23:12:54 +0530

    Add project files.

---
+++ Demo/Controllers/TestController.cs
@@ -0,0 +1,99 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Work;
+
+namespace Demo.Controllers
+{
+    public class TestController : Controller
+    {
+        Calculate Calculate = new Calculate();
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Data cal)
+        {
+            ViewBag.Result = Calculate.Add(cal.Value1, cal.Value2);
+            return View("Result");
+        }
+
+        // GET: Test/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Test/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Test/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Test/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Test/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Test/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Test/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Work;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Calculate _cal = new Calculate();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int userInput1 = 12;
+            int userInput2 = 10;
+            int result = _cal.Add(userInput1, userInput2);
+            Assert.AreEqual(22, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string userInput1 = "12";
+            string userInput2 = "10";
+            var driver = new OpenQA.Selenium.IE.InternetExplorerDriver();
+            driver.Navigate().GoToUrl("http://localhost/demo/test");
+            driver.FindElementByXPath("//*[@id='Value1']").SendKeys(userInput1);
+            driver.FindElementByXPath("//*[@id='Value2']").SendKeys(userInput2);
+            driver.FindElementByXPath("/html/body/div[2]/form/div/div[3]/div/input").SendKeys(OpenQA.Selenium.Keys.Enter);
+            Thread.Sleep(2000);
+            var result = driver.FindElementByXPath("/html/body/div[2]/h2").Text;
+            Assert.AreEqual("22", result);
+            driver.Dispose();
+        }
+    }
+}
